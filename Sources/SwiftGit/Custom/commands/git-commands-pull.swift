cc03d7ec9a72bb64ffb56d1ba337aac87fad7931
linhey <is.linhey@outlook.com>
2022-05-16 16:28:09 +0800

    update

--- Sources/SwiftGit/Custom/commands/git-commands-pull.swift
+++ Sources/SwiftGit/Custom/commands/git-commands-pull.swift
@@ -13,17 +13,17 @@ public extension Repository {
     /// https://git-scm.com/docs/git-pull
     @discardableResult
     func pull(_ options: [PullOptions] = [], refspecs: [Reference]) throws -> String {
-        try Git.run(options.map(\.rawValue) + refspecs.map(\.name), executable: .pull)
+        try run(options.map(\.rawValue) + refspecs.map(\.name), executable: .pull)
     }
     
     @discardableResult
     func pull(_ options: [PullOptions] = []) throws -> String {
-        try Git.run(options.map(\.rawValue), executable: .pull)
+        try run(options.map(\.rawValue), executable: .pull)
     }
     
     @discardableResult
     func pull(_ cmd: String) throws -> String {
-        try Git.run(cmd.split(separator: " ").map(\.description), executable: .pull)
+        try run(cmd.split(separator: " ").map(\.description), executable: .pull)
     }
     
 }
