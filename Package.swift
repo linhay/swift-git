aab674e968d09ce4198ab6df1db9078ff18932a7
linhey <is.linhey@outlook.com>
2022-06-10 16:34:44 +0800

    chore: 降低版本

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGit",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(name: "SwiftGit", targets: ["SwiftGit"]),
     ],
