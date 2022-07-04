23e130b314dcc4759915e2a1281c5e8ff1f2a6f7
linhey <is.linhey@outlook.com>
2022-07-04 18:33:38 +0800

    chore: 同步函数

--- Sources/SwiftGit/Custom/GitShell.swift
+++ Sources/SwiftGit/Custom/GitShell.swift
@@ -43,45 +43,38 @@ public extension GitShell {
         let data = try await data(exec, commands, context: context)
         return String(data: data, encoding: .utf8) ?? ""
     }
+
+    @discardableResult
+    static func string(_ exec: URL, _ commands: [String], context: Context? = nil) throws -> String {
+        let data = try data(exec, commands, context: context)
+        return String(data: data, encoding: .utf8) ?? ""
+    }
+
+    @discardableResult
+    static func data(_ exec: URL, _ commands: [String], context: Context? = nil) throws -> Data {
+        let process = self.setupProcess(exec, commands, context: context)
+        let outputPipe = pipe(for: &process.standardOutput, callback: context?.standardOutput)
+        let errorPipe  = pipe(for: &process.standardError, callback: context?.standardError)
+        try process.run()
+        process.waitUntilExit()
+        return try result(process, errorPipe, outputPipe).get()
+    }
     
     @discardableResult
     static func data(_ exec: URL, _ commands: [String], context: Context? = nil) async throws -> Data {
-       try await withUnsafeThrowingContinuation { continuation in
-           do {
-               let process = Process()
-               process.executableURL = exec
-               process.arguments = commands
-               process.currentDirectoryURL = context?.currentDirectory
-               
-               if let environment = context?.environment, !environment.isEmpty {
-                   process.environment = environment
-               }
-               
-               let outputPipe = pipe(for: &process.standardOutput, callback: context?.standardOutput)
-               let errorPipe  = pipe(for: &process.standardError, callback: context?.standardError)
-               
-               process.terminationHandler = { process in
-                   if process.terminationStatus != .zero {
-                       if let message = String(data: errorPipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8) {
-                           continuation.resume(throwing: GitError.processFatal(message))
-                       }
-                       
-                       var message = [String]()
-                       if let currentDirectory = process.currentDirectoryURL?.path {
-                           message.append("currentDirectory: \(currentDirectory)")
-                       }
-                       message.append("reason: \(process.terminationReason)")
-                       message.append("code: \(process.terminationReason.rawValue)")
-                       continuation.resume(throwing: GitError.processFatal(message.joined(separator: "\n")))
-                   }
-                   
-                   let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
-                   continuation.resume(with: .success(data))
-               }
-               try process.run()
-           } catch {
-               continuation.resume(throwing: error)
-           }
+        try await withUnsafeThrowingContinuation { continuation in
+            do {
+                let process = self.setupProcess(exec, commands, context: context)
+                let outputPipe = pipe(for: &process.standardOutput, callback: context?.standardOutput)
+                let errorPipe  = pipe(for: &process.standardError, callback: context?.standardError)
+                
+                process.terminationHandler = { process in
+                    continuation.resume(with: result(process, errorPipe, outputPipe))
+                }
+                try process.run()
+            } catch {
+                continuation.resume(throwing: error)
+            }
         }
     }
     
@@ -89,6 +82,37 @@ public extension GitShell {
 
 
 private extension GitShell {
+    
+    static func result(_ process: Process, _ errorPipe: Pipe, _ outputPipe: Pipe) -> Result<Data, Error> {
+        if process.terminationStatus != .zero {
+            if let message = String(data: errorPipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8) {
+                return .failure(GitError.processFatal(message))
+            }
+            
+            var message = [String]()
+            if let currentDirectory = process.currentDirectoryURL?.path {
+                message.append("currentDirectory: \(currentDirectory)")
+            }
+            message.append("reason: \(process.terminationReason)")
+            message.append("code: \(process.terminationReason.rawValue)")
+            return .failure(GitError.processFatal(message.joined(separator: "\n")))
+        }
+        
+        let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        return .success(data)
+    }
+    
+    static func setupProcess(_ exec: URL, _ commands: [String], context: Context? = nil) -> Process {
+        let process = Process()
+        process.executableURL = exec
+        process.arguments = commands
+        process.currentDirectoryURL = context?.currentDirectory
+        
+        if let environment = context?.environment, !environment.isEmpty {
+            process.environment = environment
+        }
+        return process
+    }
     
     static func pipe(for standard: inout Any?, callback: ((String) -> Void)?) -> Pipe {
         let pipe = Pipe()
