3b6865ba01aca28ad47fb98a8c6d50adbc416e7e
linhey <is.linhey@outlook.com>
2022-07-24 23:30:19 +0800

    chore: tags

--- Sources/SwiftGit/Custom/commands/git-commands-tag.swift
+++ Sources/SwiftGit/Custom/commands/git-commands-tag.swift
@@ -10,23 +10,33 @@ import Combine
 
 /// https://git-scm.com/docs/git-tag
 public extension Repository {
-    
+    func tagPublisher(_ options: [TagOptions] = [], tagname: String? = nil) -> AnyPublisher<String, GitError> {
+        runPublisher(["tag"] + options.map(\.rawValue) + (tagname == nil ? [] : [tagname!]))
+    }
     func tagPublisher(_ cmd: String) -> AnyPublisher<String, GitError> {
         runPublisher("tag " + cmd)
     }
-    
 }
 
 public extension Repository {
     
     @discardableResult
+    func tag(_ options: [TagOptions] = [], tagname: String? = nil) async throws -> String {
+        try await run(["tag"] + options.map(\.rawValue) + (tagname == nil ? [] : [tagname!]))
+    }
+    
+    @discardableResult
     func tag(_ cmd: String) async throws -> String {
         try await run("tag " + cmd)
     }
-    
 }
 
 public extension Repository {
+    
+    @discardableResult
+    func tag(_ options: [TagOptions] = [], tagname: String? = nil) throws -> String {
+        try run(["tag"] + options.map(\.rawValue) + (tagname == nil ? [] : [tagname!]))
+    }
     
     @discardableResult
     func tag(_ cmd: String) throws -> String {
